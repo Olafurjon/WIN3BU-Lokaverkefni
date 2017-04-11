69ddfaeb5cfffc5d911e95032c5230a853f8d0d6
Olafurjon <olafurjonb@hotmail.com>
2017-04-11 18:44:26 +0000

    Netkortsformið komið með virkni og er ready

--- gui.ps1
+++ gui.ps1
@@ -1,24 +1,154 @@
-﻿Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+﻿
 
 #býr til label með staðsettum texta og staðsetningu
+function labelmaker{
+param(
+[Parameter(Mandatory)]
+$text,
+[Parameter(Mandatory)]
+$location
+)
+
+$label = New-Object System.Windows.Forms.Label
+$label.Text = $text
+$label.TextAlign = "TopCenter"
+$label.Location = New-Object System.Drawing.Size($location[0],$location[1])
+return $label
 
 
+}
+
+function buttonmaker{
+param(
+[Parameter(Mandatory)]
+$text,
+[Parameter(Mandatory)]
+$location
+)
+
+$label = New-Object System.Windows.Forms.button
+$label.Text = $text
+$label.TextAlign = "TopCenter"
+$label.Location = New-Object System.Drawing.Size($location[0],$location[1])
+return $label
+
+
+}
+
+function tbmaker{
+param(
+[Parameter(Mandatory)]
+$size,
+[Parameter(Mandatory)]
+$location
+)
+
+$textbox = New-Object System.Windows.Forms.TextBox
+$textbox.Size = New-Object System.Drawing.Size($size[0],$size[1])
+$textbox.Location = New-Object System.Drawing.Size($location[0],$location[1])
+return $textbox
+
+}
+
+#region netkortsform
+$Script:netselectedindex = 0
 function netformtextupdate{
-$info = $Script:selectednetkort[$combo.SelectedIndex]
-$prefix = Get-NetIPAddress | Where-Object interfaceIndex -EQ $info.InterfaceIndex | Select-Object PrefixLength
+    $Script:netselectedindex = $combo.SelectedIndex
+    $info = $Script:selectednetkort[$combo.SelectedIndex]
+    $prefix = Get-NetIPAddress | Where-Object interfaceIndex -EQ $info.InterfaceIndex | Select-Object PrefixLength
 
-$netformIAinfo.Text = $info.InterfaceAlias
-$netformIP4info.Text = $info.IPv4Address
-if($info.IPv6Address.Count -eq 0){
-$netformIP6info.Text =  "Not Connected"
-$netformPrefinfo.Text = $prefix[1].PrefixLength
+    $netformIAinfo.Text = $info.InterfaceAlias
+    $netformIP4info.Text = $info.IPv4Address
+    if($info.IPv6Address.Count -eq 0){
+        $netformIP6info.Text =  "Not Connected"
+        $netformPrefinfo.Text = $prefix[1].PrefixLength
+    }
+    else{
+        $netformIP6info.Text = $info.IPv6Address
+        $netformPrefinfo.Text = $prefix[0].PrefixLength
+    }
+    if($info.DNSServer[0].ServerAddresses.Count -eq 0){
+    $netformDNSinfo.Text = $info.DNSServer[1].Address
+    }
+    else{
+    $netformDNSinfo.Text = $info.DNSServer[0].Address}
+}
+
+function breytanetkorti{
+$error.clear()
+$message = @()
+$gamlanetkort = $netformIAinfo.Text
+if($netformIA.Text.Length -eq 0){
+    $nyjanetkort = $gamlanetkort
+    }
+else{
+    $nyjanetkort = $netformIA.Text
+}
+
+if($netformIP4.Text.Length -eq 0)
+{
+    $ipaddress = $netformIP4info.Text
 }
 else{
-$netformIP6info.Text = $info.IPv6Address
-$netformPrefinfo.Text = $prefix[0].PrefixLength
-}
+    $ipaddress = $netformIP4.Text}
 
-$netformDNSinfo.Text = $info.DNSServer
+if($netformpref.Text.Length -eq 0){
+    $prefix = $netformPrefinfo.Text}
+else {
+    $prefix = $netformpref.Text}
+
+if($netformDNS.Text.Length -eq 0){
+$dnsserver = $netformDNSinfo.Text
+}
+else
+{$dnsserver = $netformDNS.Text}
+
+Try{
+    Rename-NetAdapter -Name $gamlanetkort -NewName $nyjanetkort #rename-ar netkoritð
+    $message += "Nafn Netkorts er $nyjanetkort"
+}
+catch{
+    $message += "Ekki var hægt að skýra Netkortið"
+}
+try{
+    New-NetIPAddress -InterfaceAlias $nyjanetkort -IPAddress $ipaddress -PrefixLength $prefix -ErrorAction - #-DefaultGateway 192.168.1.1 notum ekki default gateway en hægt að kommenta þetta aftur inn ef þess þarf
+    $message += "Upplýsingar $nyjanetkort , Ip: $ipaddress , Prefix: $prefix"
+}
+catch{
+$message += "Breyta Ip tölu mistókst vegna $error"
+}
+try{
+Set-DnsClientServerAddress -InterfaceAlias $nyjanetkort -ServerAddresses $dnsserver #þetta 127.0.0.1 er að setja okkur á loopback semsagt við erum okkar eigin dns þjónn
+$message += "DNS server $nyjanetkort = $dnsserver"
+}
+catch{
+$message+= "Ekki tókst að breyta DNS server"
+}
+$combo.Items.Clear()
+$allnets = Get-NetIPConfiguration -Detailed
+$net = @()
+foreach($n in $allnets){
+if($n.InterfaceAlias -like "*Loopback Pseudo-Interface*")
+{
+
+}
+else{
+$net += $n
+}
+}
+$Script:selectednetkort = $net
+
+
+foreach($n in $net){
+    if($n.AddressFamily -like "IPv6"){
+     #$combo.Items.Add($n.InterfaceAlias + " IPv6")
+    }
+    else{
+        $combo.Items.Add($n.InterfaceAlias )
+    }
+}
+$combo.SelectedIndex = $Script:netselectedindex
+netformtextupdate
 }
 
 $ctrlnetform = @()
@@ -29,6 +159,7 @@ $netform.StartPosition = "CenterScreen"
 $netform.ClientSize = New-Object System.Drawing.Size(757,315)
 $netform.add_Closed({$mainform.Show()
 $netform.Close()
+
 })
 
 #Comboboxið fyrir netform heldur yfir öll local og virtual netkort
@@ -86,17 +217,21 @@ $info = $Script:selectednetkort[$combo.SelectedIndex]
 $prefix = Get-NetIPAddress | Where-Object interfaceIndex -EQ $info.InterfaceIndex | Select-Object PrefixLength
 
 $netformIAinfo = tbmaker -size (138,20) -location (530,50)
+$netformIAinfo.ReadOnly = $true
 $netformIAinfo.Text = $info.InterfaceAlias
 $ctrlnetform += $netformIAinfo
 
 $netformIP4info = tbmaker -size (138,20) -location (530,75)
+$netformIP4info.ReadOnly = $true
 $netformIP4info.Text = $info.IPv4Address
 $ctrlnetform += $netformIP4info
 
 $netformPrefinfo = tbmaker -size (138,20) -location (530,125)
+$netformPrefinfo.ReadOnly = $true
 $ctrlnetform += $netformPrefinfo
 
 $netformIP6info = tbmaker -size (138,20) -location (530,100)
+$netformIP6info.ReadOnly = $true
 if($info.IPv6Address.Count -eq 0){
     $netformIP6info.Text =  "Not Connected"
     $netformPrefinfo.Text = $prefix[1].PrefixLength
@@ -109,11 +244,14 @@ else{
 
 $ctrlnetform += $netformIP6info
 
-$netformPrefinfo = tbmaker -size (138,20) -location (530,125)
-$ctrlnetform += $netformPrefinfo
 
 $netformDNSinfo = tbmaker -size (138,20) -location (530,150)
-$netformDNSinfo.Text = $info.DNSServer
+$netformDNSinfo.ReadOnly = $true
+    if($info.DNSServer[0].ServerAddresses.Count -eq 0){
+    $netformDNSinfo.Text = $info.DNSServer[1].Address
+    }
+    else{
+    $netformDNSinfo.Text = $info.DNSServer[0].Address}
 $ctrlnetform += $netformDNSinfo
 
 
@@ -140,7 +278,7 @@ $ctrlnetform += $netformlbl5
 $netformlbl6 = labelmaker -text "DNSserver:" -location (1,150)
 $ctrlnetform += $netformlbl6
 
-$netformlbl2 = labelmaker -text "Upplýsingar Um Netkort" -location (478,7)
+$netformlbl2 = labelmaker -text "Upplýsingar Um Netkort" -location (528,7)
 $ctrlnetform += $netformlbl2
 
 $netformlbl3 = labelmaker -text "Interface Alias:" -location (442,50)
@@ -158,6 +296,14 @@ $ctrlnetform += $netformlbl5
 $netformlbl6 = labelmaker -text "DNSserver:" -location (442,150)
 $ctrlnetform += $netformlbl6
 
+$netformbtn = buttonmaker -text "Breyta" -location (85,186)
+$netformbtn.Size = New-Object System.Drawing.Size (138,25)
+$netformbtn.add_Click({
+breytanetkorti
+netformtextupdate
+})
+$ctrlnetform += $netformbtn
+
 
 
 foreach($item in $ctrlnetform)
@@ -166,55 +312,10 @@ foreach($item in $ctrlnetform)
 }
 
 
-
-function labelmaker{
-param(
-[Parameter(Mandatory)]
-$text,
-[Parameter(Mandatory)]
-$location
-)
-
-$label = New-Object System.Windows.Forms.Label
-$label.Text = $text
-$label.TextAlign = "TopCenter"
-$label.Location = New-Object System.Drawing.Size($location[0],$location[1])
-return $label
+#endregion
 
 
-}
 
-function buttonmaker{
-param(
-[Parameter(Mandatory)]
-$text,
-[Parameter(Mandatory)]
-$location
-)
-
-$label = New-Object System.Windows.Forms.button
-$label.Text = $text
-$label.TextAlign = "TopCenter"
-$label.Location = New-Object System.Drawing.Size($location[0],$location[1])
-return $label
-
-
-}
-
-function tbmaker{
-param(
-[Parameter(Mandatory)]
-$size,
-[Parameter(Mandatory)]
-$location
-)
-
-$textbox = New-Object System.Windows.Forms.TextBox
-$textbox.Size = New-Object System.Drawing.Size($size[0],$size[1])
-$textbox.Location = New-Object System.Drawing.Size($location[0],$location[1])
-return $textbox
-
-}
 
 #region byrjun
 $ctrlsmainform = @()
